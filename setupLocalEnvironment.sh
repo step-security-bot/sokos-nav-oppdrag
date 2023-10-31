be9fef73cb6711256903369cc966598995c0706a
jon disen <jon.disen@nav.no>
2023-10-31 15:35:07 +0100

    Lagt inn referanse til secrets i naiserator og tilpasset setupLocalEnvironment for oppdrag-database-credentials

--- setupLocalEnvironment.sh
+++ setupLocalEnvironment.sh
@@ -10,13 +10,19 @@ fi
 kubectl config use-context dev-fss
 kubectl config set-context --current --namespace=okonomi
 
-# Get database username and password secret from Vault
-[[ "$(vault token lookup -format=json | jq '.data.display_name' -r; exit ${PIPESTATUS[0]})" =~ "nav.no" ]] &>/dev/null || vault login -method=oidc -no-print
+# Login to Vault to fetch credentials
+#[[ "$(vault token lookup -format=json | jq '.data.display_name' -r; exit ${PIPESTATUS[0]})" =~ "nav.no" ]] &>/dev/null || vault login -method=oidc -no-print
+
+# Get database username and password from secret
+DATABASE_USERNAME=$(kubectl -n okonomi get secret oppdrag-credentials -o jsonpath="{ .data['DATABASE_USERNAME'] }" | base64 -d)
+DATABASE_PASSWORD=$(kubectl -n okonomi get secret oppdrag-credentials -o jsonpath="{ .data['DATABASE_PASSWORD'] }" | base64 -d)
 
 # Get AZURE and DATABASE system variables
-envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-skattekort-person | cut -f1 -d' ') -c sokos-skattekort-person -- env | egrep "^AZURE")
+envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-nav-oppdrag | cut -f1 -d' ') -c sokos-nav-oppdrag -- env | egrep "^AZURE")
 
 # Set AZURE as local environment variables
 rm -f defaults.properties
 echo "$envValue" > defaults.properties
+echo "DATABASE_USERNAME=$DATABASE_USERNAME" >> defaults.properties
+echo "DATABASE_PASSWORD=$DATABASE_PASSWORD" >> defaults.properties
 echo "AZURE env variables stores as defaults.properties"
