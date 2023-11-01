2eab4db59e97a47422c6959827757a104192ed14
jon disen <jon.disen@nav.no>
2023-11-01 08:40:13 +0100

    Fjernet unødvendige dependencies

--- oppdragsinfo/build.gradle.kts
+++ oppdragsinfo/build.gradle.kts
@@ -1,22 +1,9 @@
 val ktorVersion = "2.3.5"
-val kotestVersion = "5.7.2"
-val mockkVersion = "1.13.8"
-val kotlin_version="1.9.10"
-val logback_version="1.4.11"
-val prometeus_version="1.11.5"
-val h2_version="2.1.214"
 val swaggerRequestValidatorVersion="2.38.1"
 val restAssuredVersion="5.3.2"
 val mockOAuth2ServerVersion="2.0.0"
-val kotlinLoggingVersion="3.0.5"
-val janionVersion="3.1.10"
-val logbackVersion="1.4.11"
-val logstashVersion="7.4"
-val papertrailappVersion="1.0.0"
-val natpryceVersion="1.6.10.0"
 val hikariVersion="5.0.1"
 val db2_jcc_version="11.5.8.0"
-val jacksonVersion="2.15.3"
 val jooqVersion="3.18.7"
 
 plugins {
@@ -26,46 +13,14 @@ plugins {
 dependencies {
 
     // Ktor server
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-request-validation:$ktorVersion")
     implementation("io.ktor:ktor-server-swagger:$ktorVersion")
     implementation("io.ktor:ktor-server-openapi:$ktorVersion")
     implementation("io.ktor:ktor-server-resources:$ktorVersion")
 
-    // Ktor client
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-apache-jvm:$ktorVersion")
-
-    // Security
-    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
-
     // Monitorering
     implementation("io.ktor:ktor-server-metrics-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
-
-    // Serialization
-    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
-
-    // Logging
-    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
-    runtimeOnly("org.codehaus.janino:janino:$janionVersion")
-    runtimeOnly("ch.qos.logback:logback-classic:$logbackVersion")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
-    runtimeOnly("com.papertrailapp:logback-syslog4j:$papertrailappVersion")
-
-    // Config
-    implementation("com.natpryce:konfig:$natpryceVersion")
 
     // Database
     implementation("org.jooq:jooq:$jooqVersion")
