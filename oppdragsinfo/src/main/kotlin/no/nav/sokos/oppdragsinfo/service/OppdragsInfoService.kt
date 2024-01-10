fadfa1cba218a41af5512e92225229f1cf00f508
mudasar187 <mudasar_cheema@hotmail.com>
2024-01-10 11:48:55 +0100

    fix merge conflict

--- oppdragsinfo/src/main/kotlin/no/nav/sokos/oppdragsinfo/service/OppdragsInfoService.kt
+++ oppdragsinfo/src/main/kotlin/no/nav/sokos/oppdragsinfo/service/OppdragsInfoService.kt
@@ -155,9 +155,9 @@ class OppdragsInfoService(
 
     private suspend fun getGjelderIdNavn(gjelderId: String): String =
         when {
-            gjelderId.length == 11 && gjelderId.toLong() > 80000000000 -> tpService.getLeverandorNavn(gjelderId).navn
-            gjelderId.length == 11 && gjelderId.toLong() < 80000000000 -> pdlService.getPersonNavn(gjelderId)?.navn?.firstOrNull()
-                ?.run { "$fornavn $mellomnavn $etternavn" } ?: ""
+            gjelderId.toLong() > 80000000000 -> tpService.getLeverandorNavn(gjelderId).navn
+            gjelderId.toLong() < 80000000000 -> pdlService.getPersonNavn(gjelderId)?.navn?.firstOrNull()
+                ?.run { mellomnavn?.let { "$fornavn $mellomnavn $etternavn" } ?: "$fornavn $etternavn" } ?: ""
 
             else -> eregService.getOrganisasjonsNavn(gjelderId).navn.sammensattnavn
         }
@@ -166,3 +166,67 @@ class OppdragsInfoService(
         return getSaksbehandler(call)
     }
 }
+
+/*    suspend fun hentOppdragslinje(
+        oppdragsId: String,
+        oppdragslinje: String,
+        applicationCall: ApplicationCall
+    ): List<OppdragslinjeVO> {
+        val saksbehandler = hentSaksbehandler(applicationCall)
+        secureLogger.info("Henter oppdrag med id: $oppdragsId")
+        auditLogger.auditLog(
+            AuditLogg(
+                saksbehandler = saksbehandler.ident,
+                oppdragsId = oppdragsId
+            )
+        )
+        return db2DataSource.connection.useAndHandleErrors { connection ->
+            connection.setAcceleration()
+            val oppdragsLinjeInfo =
+                finnOppdragslinjeInfo(connection, oppdragsId.trim().toInt(), oppdragslinje.trim().toInt())
+
+            oppdragsLinjeInfo.oppdragslinjer.map {
+                OppdragslinjeVO(
+                    it.oppdragsId,
+                    it.linjeId,
+                    it.delytelseId,
+                    it.sats,
+                    it.typeSats,
+                    it.vedtakFom.orEmpty(),
+                    it.vedtakTom.orEmpty(),
+                    it.attestert,
+                    it.vedtaksId,
+                    it.utbetalesTilId,
+                    it.refunderesOrgnr.orEmpty(),
+                    it.brukerid,
+                    it.tidspktReg,
+                    oppdragsLinjeInfo.skyldnere,
+                    oppdragsLinjeInfo.valutaer,
+                    oppdragsLinjeInfo.linjeenheter,
+                    oppdragsLinjeInfo.kidliste,
+                    oppdragsLinjeInfo.tekster,
+                    oppdragsLinjeInfo.grader,
+                    oppdragsLinjeInfo.kravhavere,
+                    oppdragsLinjeInfo.maksdatoer
+                )
+            }
+        }
+    } */
+
+/*    suspend fun finnOppdragslinjeInfo(connection: Connection, oppdragsId: Int, linjeId: Int): OppdragsLinjeInfo {
+        val oppdLinje: OppdragsLinjeInfo
+        coroutineScope {
+            oppdLinje = OppdragsLinjeInfo(
+                async { connection.hentOppdragslinje(oppdragsId, linjeId) }.await(),
+                async { connection.hentSkyldnere(oppdragsId, linjeId) }.await(),
+                async { connection.hentValutaer(oppdragsId, linjeId) }.await(),
+                async { connection.hentLinjeenheter(oppdragsId, linjeId) }.await(),
+                async { connection.hentKidlister(oppdragsId, linjeId) }.await(),
+                async { connection.henOppdragsTekster(oppdragsId, linjeId) }.await(),
+                async { connection.hentGrader(oppdragsId, linjeId) }.await(),
+                async { connection.henKravhavere(oppdragsId, linjeId) }.await(),
+                async { connection.henMaksdatoer(oppdragsId, linjeId) }.await()
+            )
+        }
+        return oppdLinje
+    }*/
