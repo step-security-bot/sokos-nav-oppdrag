7e01db14d53de99d6b990d5d4a33b50d47460f5f
jon disen <jon.disen@nav.no>
2024-01-17 08:26:57 +0100

    Implementert oppdragsenheter i hentOppdrag

--- oppdragsinfo/src/main/kotlin/no/nav/sokos/oppdragsinfo/api/model/OppdragsResponse.kt
+++ oppdragsinfo/src/main/kotlin/no/nav/sokos/oppdragsinfo/api/model/OppdragsResponse.kt
@@ -1,10 +1,13 @@
 package no.nav.sokos.oppdragsinfo.api.model
 
 import kotlinx.serialization.Serializable
+import no.nav.sokos.oppdragsinfo.domain.OppdragsEnhet
 import no.nav.sokos.oppdragsinfo.domain.OppdragsLinje
 
 @Serializable
 data class OppdragsResponse(
+    val enhet: OppdragsEnhet,
+    val behandlendeEnhet: OppdragsEnhet? = null,
     val harOmposteringer: Boolean,
     val oppdragsLinjer: List<OppdragsLinje>
 )
